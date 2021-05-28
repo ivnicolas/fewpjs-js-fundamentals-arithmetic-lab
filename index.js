7b6e68560cc6216260dad6b20ae19809d21c1f4a
ivnicolas <ikennanicolas@gmail.com>
2021-05-28 16:59:34 -0400

    commit tree 279719d2b1d4bebdcec889c230201d8f7bcfea2c

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 5;
+let num4= 29;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 21) + 5;
+
+let mod = num4 % num3
+
+let max = Math.max(5,10,15,20)
